7b04a732abbc551e1a19d70e050c59c8faf82757
BattleIsOver <34491874+AleMulasDev@users.noreply.github.com>
2023-02-05 16:58:15 +0100

    Empty base pages

---
+++ src/views/ListaEserciziPage.vue
@@ -0,0 +1,14 @@
+<template>
+	<EmptyContainer name="Lista esercizi">
+		
+	</EmptyContainer>
+</template>
+
+<script lang="ts" setup>
+import { useSchedeStore } from '@/stores/schede';
+import EmptyContainer from '@/components/EmptyContainer.vue';
+import { ref } from 'vue';
+
+const sStore = ref(useSchedeStore())
+
+</script>
---
+++ src/views/PrenotaSalaPage.vue
@@ -0,0 +1,14 @@
+<template>
+	<EmptyContainer name="Prenota Sala">
+		
+	</EmptyContainer>
+</template>
+
+<script lang="ts" setup>
+import { useSchedeStore } from '@/stores/schede';
+import EmptyContainer from '@/components/EmptyContainer.vue';
+import { ref } from 'vue';
+
+const sStore = ref(useSchedeStore())
+
+</script>

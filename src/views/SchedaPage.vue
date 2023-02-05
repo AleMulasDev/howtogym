fc17f33295b4707e45bb75eb92395bcfb8e03a9b
BattleIsOver <34491874+AleMulasDev@users.noreply.github.com>
2023-02-05 17:48:04 +0100

    Base exercise view

--- src/views/SchedaPage.vue
+++ src/views/SchedaPage.vue
@@ -18,19 +18,63 @@
 			<EsercizioCard v-for="es of scheda.esercizi" :key="es.nome" :esercizio="es"></EsercizioCard>
 		</div>
 
-		<ion-button slot="fixed" style="width: 128px;">
+		<ion-button slot="fixed" style="width: 128px;" :id="`open_modal_${scheda.nome}`">
 			Inizia allenamento
 		</ion-button>
+
+		<div v-if="scheda">
+			<ion-modal ref="modal" :trigger="`open_modal_${scheda.nome}`">
+			<ion-header>
+				<ion-toolbar>
+				<ion-title>Allenamento</ion-title>
+				<ion-buttons slot="end">
+					<ion-button @click="dismiss()">Close</ion-button>
+				</ion-buttons>
+				</ion-toolbar>
+			</ion-header>
+			<ion-content class="ion-padding">
+				<div class="flex flex-row w-full justify-center">
+					<div @click="esNumber--" class="mr-3">
+						<ion-button>Indietro</ion-button>
+					</div>
+					<div @click="esNumber++" class="ml-3">
+						<ion-button>Avanti</ion-button>
+					</div>
+				</div>
+				<div v-for="(esercizio, index) in scheda.esercizi" :key="esercizio.esercizio">
+					<div v-if="esNumber == index">
+						<div class="w-full mt-5 mb-5">
+							<ion-img :src="`/assets/GIFs/${esercizio?._esercizio?.gif}`" class="overflow-hidden w-2/3 h-auto rounded-2xl mx-auto"></ion-img>
+						</div>
+						<div class="text-center text-lg font-bold">
+							
+						</div>
+						<div class="bg-slate-900 flex flex-row w-full p-5 rounded-2xl">
+							<div class="flex items-center h-full">
+								{{ esercizio.esercizio }}
+							</div>
+							<div class="flex ml-auto">
+								<ion-img :src="`/assets/GIFs/${esercizio?._esercizio?.gif}`" class="overflow-hidden w-20 h-20 rounded-2xl mx-auto"></ion-img>
+							</div>
+						</div>
+						<div class="bg-slate-900 p-5 mt-5 rounded-2xl">
+							{{ esercizio?._esercizio?.descrizione }}
+						</div>
+					</div>
+				</div>
+			</ion-content>
+			</ion-modal>
+		</div>
 	</EmptyContainer>
 </template>
 
 <script lang="ts" setup>
-import { IonImg, useIonRouter, IonIcon, IonButton } from '@ionic/vue';
+import { IonImg, useIonRouter, IonIcon, IonButton, IonModal, IonContent, IonHeader, IonButtons, IonToolbar, IonTitle } from '@ionic/vue';
 import { star, starOutline, calendarClearOutline } from 'ionicons/icons';
 import { useRoute } from 'vue-router';
 import { useSchedeStore } from '@/stores/schede';
 import EsercizioCard from '@/components/EsercizioCard.vue'
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import EmptyContainer from '@/components/EmptyContainer.vue';
 
 const route = useRoute()
@@ -44,6 +88,15 @@ const icon = computed(() => {
 const toggleFav = () => {
 	schedeS.toggleFavScheda(scheda)
 	scheda.isFavourite = !scheda.isFavourite
+}
+
+const esNumber = ref(0)
+const presenting = ref(false)
+const modal = ref(null)
+
+function dismiss(){
+	presenting.value = false
+	modal.value.$el.dismiss();
 }
 
 const scheda = reactive(schedeS.schede.filter(s => s.id == route.params.id)[0])
